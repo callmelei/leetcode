cba0635b20335f413f96518c47a0d1b54eaf0f4c
Lei Zhang <callmelei@outlook.com>
2021-08-08 10:22:13 -0400

    Two-sum in O(n), first round

--- 1.two-sum.java
+++ 1.two-sum.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /*
  * @lc app=leetcode id=1 lang=java
  *
@@ -7,13 +9,12 @@
 // @lc code=start
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target)
-                    return new int[] { i, j };
-            }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; map.put(nums[i], i++)) {
+            if (map.containsKey(target - nums[i]))
+                return new int[] { map.get(target - nums[i]), i };
         }
-        return new int[] {};
+        return new int[2];
     }
 }
 // @lc code=end
